4ea29102011784e6ffaaad86aa8b7c165835a678
BlueNguyen <bangtam099589@gmail.com>
2023-12-30 00:44:41 +0700

    Modify vallidation

--- js/main.js
+++ js/main.js
@@ -15,6 +15,17 @@ function addEmployee() {
   let hoursWork = +DOMid("gioLam").value;
   //   console.log({ account, name, email, pass, dateWork, base, pos, hoursWork });
 
+  // Kiểm tra Điều kiện
+  validateAccount();
+  validateName();
+  validateEmail();
+  validatePass();
+  validateDateWork();
+  validateBase();
+  validatePos();
+  validateHoursWork();
+
+
   // Tạo object employee
   let employee = {
     account: account,
@@ -32,11 +43,11 @@ function addEmployee() {
   function tinhLuong() {
     let tongLuong = 0;
     if (employee.pos == "Giám đốc") {
-      tongLuong = employee.base * 3 * employee.hoursWork;
+      tongLuong = employee.base * 3;
     } else if (employee.pos == "Trưởng phòng") {
-      tongLuong = employee.base * 2 * employee.hoursWork;
+      tongLuong = employee.base * 2;
     } else {
-      tongLuong = employee.base * employee.hoursWork;
+      tongLuong = employee.base;
     }
     employee.tongLuong = tongLuong.toLocaleString("en-US") + " đ";
   }
@@ -58,55 +69,49 @@ function addEmployee() {
   }
   xepLoai();
 
-  // Kiểm tra Điều kiện
-  validateAccount();
 
-  // Thêm đối tượng Nhân viên vào bảng khi trang được tải
-  if (accErr == "true") {
-    addEmployeeToTable(employee);
+  function addEmployeeToTable(employee) {
+    // Tạo một hàng mới cho nhân viên
+    const newRow = document.createElement("tr");
+
+    //   Tạo cột Tài khoản và thêm vào hàng
+    const account = document.createElement("td");
+    account.textContent = employee.account;
+    newRow.appendChild(account);
+
+    // Tạo cột Họ tên và thêm vào hàng
+    const name = document.createElement("td");
+    name.textContent = employee.name;
+    newRow.appendChild(name);
+
+    // Tạo cột email và thêm vào hàng
+    const email = document.createElement("td");
+    email.textContent = employee.email;
+    newRow.appendChild(email);
+
+    // Tạo cột ngày làm và thêm vào hàng
+    const dateWork = document.createElement("td");
+    dateWork.textContent = employee.dateWork;
+    newRow.appendChild(dateWork);
+
+    // Tạo cột Chức vụ và thêm vào hàng
+    const pos = document.createElement("td");
+    pos.textContent = employee.pos;
+    newRow.appendChild(pos);
+
+    // Tạo cột Tổng lương và thêm vào hàng
+    const tongLuong = document.createElement("td");
+    tongLuong.textContent = employee.tongLuong;
+    newRow.appendChild(tongLuong);
+
+    //   Tạo cột Xếp loại và thêm vào hàng
+    const xepLoai = document.createElement("td");
+    xepLoai.textContent = employee.xepLoai;
+    newRow.appendChild(xepLoai);
+
+    // Thêm hàng vào tbody
+    const tbody = document.getElementById("tableDanhSach");
+    tbody.appendChild(newRow);
   }
-}
-
-function addEmployeeToTable(employee) {
-  // Tạo một hàng mới cho nhân viên
-  const newRow = document.createElement("tr");
-
-  //   Tạo cột Tài khoản và thêm vào hàng
-  const account = document.createElement("td");
-  account.textContent = employee.account;
-  newRow.appendChild(account);
-
-  // Tạo cột Họ tên và thêm vào hàng
-  const name = document.createElement("td");
-  name.textContent = employee.name;
-  newRow.appendChild(name);
-
-  // Tạo cột email và thêm vào hàng
-  const email = document.createElement("td");
-  email.textContent = employee.email;
-  newRow.appendChild(email);
-
-  // Tạo cột ngày làm và thêm vào hàng
-  const dateWork = document.createElement("td");
-  dateWork.textContent = employee.dateWork;
-  newRow.appendChild(dateWork);
-
-  // Tạo cột Chức vụ và thêm vào hàng
-  const pos = document.createElement("td");
-  pos.textContent = employee.pos;
-  newRow.appendChild(pos);
-
-  // Tạo cột Tổng lương và thêm vào hàng
-  const tongLuong = document.createElement("td");
-  tongLuong.textContent = employee.tongLuong;
-  newRow.appendChild(tongLuong);
-
-  //   Tạo cột Xếp loại và thêm vào hàng
-  const xepLoai = document.createElement("td");
-  xepLoai.textContent = employee.xepLoai;
-  newRow.appendChild(xepLoai);
-
-  // Thêm hàng vào tbody
-  const tbody = document.getElementById("tableDanhSach");
-  tbody.appendChild(newRow);
+  addEmployeeToTable(employee);
 }
--- js/validation.js
+++ js/validation.js
@@ -1,26 +1,137 @@
 // Kiểm tra Điều kiện Input
 
-function validateAccount() {
+function validateAccount(event) {
   let accountInput = document.getElementById("tknv");
   let accountError = document.getElementById("tbTKNV");
   let accountValue = accountInput.value.trim();
-  let accErr = "true";
 
   console.log(accountInput);
 
   // Kiểm tra độ dài của tài khoản
   if (accountValue.length === 0) {
-    accountError.textContent = "Tài khoản không được để trống.";
-    accErr = "false";
+    accountError.textContent = "Tài khoản không được để trống";
+    event.preventDefault();
   } else if (accountValue.length < 4 || accountValue.length > 6) {
-    accountError.textContent = "Tài khoản phải có độ dài từ 4 đến 6 ký tự.";
-    accErr = "false";
+    accountError.textContent = "Tài khoản phải có độ dài từ 4 đến 6 ký tự";
+    event.preventDefault();
   } else {
     // Nếu tài khoản hợp lệ, xóa thông báo lỗi
     accountError.textContent = "";
-    accErr = "true";
   }
-  console.log(accountError.textContent);
-  console.log("accErr",accErr);
-  return accErr;
+}
+
+function validateName(event) {
+  let nameInput = document.getElementById("name");
+  let nameError = document.getElementById("tbTen");
+  let nameValue = nameInput.value.trim();
+  console.log(nameInput);
+
+  if (nameValue.length === 0) {
+    nameError.textContent = "Tên không được để trống";
+    event.preventDefault();
+  } else if (!/^[a-z A-Z]+$/.test(nameValue)) {
+    nameError.textContent = "Tên phải là chữ";
+    event.preventDefault();
+  } else {
+    nameError.textContent = "";
+  }
+}
+
+function validateEmail(event) {
+  let emailInput = document.getElementById("email");
+  let emailError = document.getElementById("tbEmail");
+  let emailValue = emailInput.value.trim();
+
+  if (emailValue.length === 0) {
+    emailError.textContent = "Email không được để trống";
+    event.preventDefault();
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue)) {
+    emailError.textContent = "Email phải đúng định dạng";
+    event.preventDefault();
+  } else {
+    emailError.textContent = "";
+  }
+}
+
+function validatePass(event) {
+  let passInput = document.getElementById("password");
+  let passError = document.getElementById("tbMatKhau");
+  let passValue = passInput.value.trim();
+
+  if (passValue.length === 0) {
+    passError.textContent = "Mật khẩu không được để trống";
+    event.preventDefault();
+  } else if (passValue.length < 6 || passValue.length > 10) {
+    passError.textContent = "Mật khẩu phải dài từ 6 đến 10 kí tự";
+    event.preventDefault();
+  } else if (
+    !/\d/.test(passValue) &&
+    !/[A-Z]/.test(passValue) &&
+    !/[!@#$%^&*]/.test(passValue)
+  ) {
+    passError.textContent =
+      "Mật khẩu phải chứa ít nhất 1 ký tự số, 1 ký tự in hoa, 1 ký tự đặc biệt";
+    console.log(passError.textContent);
+    // event.preventDefault();
+  } else {
+    passError.textContent = "";
+  }
+}
+
+function validateDateWork(event) {
+  let dateInput = document.getElementById("datepicker");
+  let dateError = document.getElementById("tbNgay");
+  let dateValue = dateInput.value.trim();
+
+  if (dateValue.length === 0) {
+    dateError.textContent = "Ngày làm không được để trống";
+    event.preventDefault();
+  } else {
+    dateError.textContent = "";
+  }
+}
+
+function validateBase(event) {
+  let baseInput = document.getElementById("luongCB");
+  let baseError = document.getElementById("tbLuongCB");
+  let baseValue = baseInput.value.trim();
+
+  if (baseValue.length === 0) {
+    baseError.textContent = "Lương cơ bản không được để trống";
+    event.preventDefault();
+  } else if (baseValue < 1000000 || baseValue > 20000000) {
+    baseError.textContent = "Lương cơ bản phải từ 1 000 000 đến 20 000 000";
+    event.preventDefault();
+  } else {
+    baseError.textContent = "";
+  }
+}
+
+function validatePos(event) {
+  let posInput = document.getElementById("chucvu");
+  let posError = document.getElementById("tbChucVu");
+  let posValue = posInput.value.trim();
+
+  if ((posValue.length = 0)) {
+    posError.textContent = "Phải chọn chức vụ hợp lệ";
+    event.preventDefault();
+  } else {
+    posError.textContent = "";
+  }
+}
+
+function validateHoursWork(event) {
+  let hoursInput = document.getElementById("gioLam");
+  let hoursError = document.getElementById("tbGiolam");
+  let hoursValue = hoursInput.value.trim();
+
+  if (hoursValue.length === 0) {
+    hoursError.textContent = "Số giờ làm trong tháng không được bỏ trống";
+    event.preventDefault();
+  } else if (hoursValue < 80 || hoursValue > 200) {
+    hoursError.textContent = "Số giờ làm phải từ 80 đến 200";
+    event.preventDefault();
+  } else {
+    hoursError.textContent = "";
+  }
 }
