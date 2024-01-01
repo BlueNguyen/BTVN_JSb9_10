fc8b450b5d27bdf8a1b92ff237b9060afdc3888e
BlueNguyen <bangtam099589@gmail.com>
2024-01-01 23:48:38 +0700

    modify

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 
-
+// let deleYes = document.getElementById("confirmDelete");
+// deleYes.addEventListener("click", function(event){
+//   let targetRowId = event.target.closest("tr").rowIndex;
+//   event.target.closest("tr").remove(targetRowId);
+// })
    // Lấy thẻ table
-  var employeeTable = document.getElementById('tableDanhSach');
+  let employeeTable = document.getElementById('tableDanhSach');
 
   // Gắn sự kiện click cho table
   employeeTable.addEventListener('click', function(event) {
     // Kiểm tra xem phần tử được click có class 'delete-icon' hay không
     if (event.target.classList.contains('deleButton')) {
       // Lấy giá trị từ cột ID (vị trí của hàng + 1)
-      var targetRowId = event.target.closest('tr').rowIndex;
+      let targetRowId = event.target.closest('tr').rowIndex;
       // Lấy giá trị từ cột Tên
-      var targetRowName = event.target.closest('tr').cells[1].innerText;
+      let targetRowName = event.target.closest('tr').cells[1].innerText;
 
       // Hiển thị thông báo xác nhận
       if (confirm("Bạn có chắc chắn muốn xoá nhân viên " + targetRowName + " không?")) {
@@ -19,3 +23,5 @@
       }
     }
   });
+
+
